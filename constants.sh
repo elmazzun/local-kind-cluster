576571d46a9374d49ba3b3661000d65a4a6420f5
a.mazzocchi <a.mazzocchi@cineca.it>
2022-10-08 23:30:27 +0200

    Exported constants to constants.sh

---
+++ constants.sh
@@ -0,0 +1,7 @@
+readonly K8S_DASHBOARD_URL="http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:dashboard-kubernetes-dashboard:https/proxy/#/login"
+readonly K8S_DASHBOARD_NAMESPACE="kubernetes-dashboard"
+
+readonly PROMETHEUS_DASHBOARD_URL="http://localhost:9090/target"
+readonly PROMETHEUS_NAMESPACE="monitoring"
+
+readonly TIMEOUT_READINESS="1h"
--- setup-cluster.sh
+++ setup-cluster.sh
@@ -4,11 +4,7 @@
 
 set -e
 
-readonly K8S_DASHBOARD_URL="http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:dashboard-kubernetes-dashboard:https/proxy/#/login"
-readonly PROMETHEUS_DASHBOARD_URL="http://localhost:9090/target"
-readonly TIMEOUT_READINESS="1h"
-
-# --for=jsonpath='{.status.phase}'=Running
+source ./constants.sh
 
 echo "[1/14] Applying ingress..."
 kubectl apply -f manifests/ingress.yaml
@@ -30,14 +26,15 @@ echo
 
 echo "[5/14] Installing k8s dashboard..."
 helm install dashboard kubernetes-dashboard/kubernetes-dashboard \
-    -n kubernetes-dashboard \
+    -n "$K8S_DASHBOARD_NAMESPACE" \
     --create-namespace
 echo
 
 echo "[6/14] Waiting for k8s dashboard to be ready..."
-kubectl wait --namespace kubernetes-dashboard \
-  --for=condition=ready pod --selector=app.kubernetes.io/component=kubernetes-dashboard \
-  --timeout="$TIMEOUT_READINESS"
+kubectl wait --namespace "$K8S_DASHBOARD_NAMESPACE" \
+    --for=condition=ready pod --selector=app.kubernetes.io/component=kubernetes-dashboard \
+    --timeout="$TIMEOUT_READINESS"
+    echo "Dashboard ready at $K8S_DASHBOARD_URL"
 echo
 
 echo "[7/14] Adding user for k8s dashboard..."
@@ -46,18 +43,18 @@ echo
 
 echo "[8/14] Getting user secret in order to login to k8s dashboard..."
 admin_user_token=$(kubectl get serviceaccount admin-user \
-    -n kubernetes-dashboard \
+    -n "$K8S_DASHBOARD_NAMESPACE" \
     -o jsonpath='{.secrets[0].name}{"\n"}')
 echo
 
 echo "[9/14] Returning user secret token: copy/paste this to k8s dashboard login page..."
 oc get secret "$admin_user_token" \
-    -n kubernetes-dashboard \
+    -n "$K8S_DASHBOARD_NAMESPACE" \
     -o jsonpath='{.data.token}' | base64 -d; echo
 echo
 
 echo "[10/14] Visit following URL and paste the returned token above..."
-echo "$DASHBOARD_URL"
+echo "$K8S_DASHBOARD_URL"
 echo
 
 echo "[11/14] Starting background k8s proxy server..."
@@ -65,8 +62,9 @@ kubectl proxy &
 echo
 
 echo "[12/14] Installing Prometheus..."
+# kill -9 $(fuser 8001/tcp 2> /dev/null | xargs echo)
 helm install --wait --timeout "$TIMEOUT_READINESS" \
-    --namespace monitoring --create-namespace \
+    --namespace "$PROMETHEUS_NAMESPACE" --create-namespace \
     --repo https://prometheus-community.github.io/helm-charts \
     kube-prometheus-stack kube-prometheus-stack \
     --values - <<EOF
@@ -77,23 +75,24 @@ EOF
 echo
 
 echo "[13/14] Waiting for Prometheus and AlertMangager pods to be ready..."
-kubectl wait --namespace=monitoring \
+kubectl wait --namespace="$PROMETHEUS_NAMESPACE" \
     --for=condition=ready pod \
     --selector=app.kubernetes.io/instance=kube-prometheus-stack \
     --timeout="$TIMEOUT_READINESS"
 
-kubectl wait --namespace=monitoring \
+kubectl wait --namespace="$PROMETHEUS_NAMESPACE" \
     --for=condition=ready pod \
     --selector=app.kubernetes.io/instance=kube-prometheus-stack-prometheus \
     --timeout="$TIMEOUT_READINESS"
 
-kubectl wait --namespace=monitoring \
+kubectl wait --namespace="$PROMETHEUS_NAMESPACE" \
     --for=condition=ready pod \
     --selector=app.kubernetes.io/instance=kube-prometheus-stack-alertmanager \
     --timeout="$TIMEOUT_READINESS"
 echo "Pods ready: Prometheus dashboard is at $PROMETHEUS_DASHBOARD_URL, no authentication is required"
+echo
 
 echo "[14/14] Port forwarding to newly created Prometheus service..."
-kubectl port-forward -n monitoring \
+kubectl port-forward -n "$PROMETHEUS_NAMESPACE" \
     svc/kube-prometheus-stack-prometheus 9090:9090
 echo
